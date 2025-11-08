d42f89893433e0e97d5d5e3d6c300c326568232a
SkylersWorld <mashoguliashvili00@gmail.com>
2025-11-08 17:00:32 +0100

    Create scene.cpp

---
+++ scene.cpp
@@ -0,0 +1,21 @@
+#include "Scene.hpp"
+
+Scene::Scene() {
+    // Constructor implementation
+}
+
+void Scene::load() {
+    // Load scene resources
+}
+
+void Scene::update() {
+    // Update scene logic
+}
+
+void Scene::render() {
+    // Render the scene
+}
+
+Scene::~Scene() {
+    // Destructor implementation
+}
