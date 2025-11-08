001a9a868f505c98f57bd0eb70bb57ffac9e310d
SkylersWorld <mashoguliashvili00@gmail.com>
2025-11-08 16:59:41 +0100

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,25 @@
+#include "Player.hpp"
+
+Player::Player() {
+    // Initialize player attributes
+}
+
+void Player::move(float deltaX, float deltaY) {
+    // Update player position based on input
+}
+
+void Player::jump() {
+    // Implement jump logic
+}
+
+void Player::attack() {
+    // Implement attack logic
+}
+
+void Player::update() {
+    // Update player state
+}
+
+void Player::render(Renderer& renderer) {
+    // Render the player using the renderer
+}
